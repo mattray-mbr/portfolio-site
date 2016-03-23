40c68c3d3f73d6d6d89a4d7831e8456e6d70b75c
mattray-mbr <mattray.mbr@gmail.com>
2016-03-22 19:55:06 -0600

    added more page structure

--- public/js/ang.js
+++ public/js/ang.js
@@ -7,4 +7,20 @@ app.config(function($routeProvider){
             templateUrl : '/html/home.html',
             controller  : 'mainController'
         })
+        .when('/about', {
+            templateUrl : '/html/about.html',
+            controller  : 'mainController'
+        })
+        .when('/projects', {
+            templateUrl : '/html/projects.html',
+            controller  : 'mainController'
+        })
+        .when('/resources', {
+            templateUrl : '/html/resources.html',
+            controller  : 'mainController'
+        })
+        .when('/contact', {
+            templateUrl : '/html/contact.html',
+            controller  : 'mainController'
+        })
 })
